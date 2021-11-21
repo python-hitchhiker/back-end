d3926ab49fc4a805109dc9fa76c462fd1cc5147c
doi-h <jeonghwanlee15@gmail.com>
2021-11-21 11:24:53 +0900

    a

---
+++ hitchhiker/database.js
@@ -0,0 +1,6 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('test-db', 'user', 'password', {
+	dialect: 'sqlite',
+	host: './dev.sqlite', // host: ':memory'
+});
+module.exports = sequelize;
